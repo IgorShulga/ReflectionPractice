cfe54d9d3afe3956d754c476dc93acbba0b63442
IgorShulga <igor.shulga8@gmail.com>
2019-07-12 19:25:57 +0300

    add test class

---
+++ src/test/java/ua/skillsup/practice/JsonParserTest.java
@@ -0,0 +1,12 @@
+package ua.skillsup.practice;
+
+class JsonParserTest {
+
+    @org.junit.jupiter.api.Test
+    void toJson() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void fromJson() {
+    }
+}
